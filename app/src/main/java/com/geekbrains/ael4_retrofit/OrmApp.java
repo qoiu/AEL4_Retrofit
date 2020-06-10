b1ffa094de1c7f351cf2a05971135be8cefc4899
Nikita <guschinnick@gmail.com>
2020-06-11 02:12:05 +0300

    stetho

--- app/src/main/java/com/geekbrains/ael4_retrofit/OrmApp.java
+++ app/src/main/java/com/geekbrains/ael4_retrofit/OrmApp.java
@@ -22,24 +22,15 @@ public class OrmApp extends Application {
         super.onCreate();
         instance=this;
         component= DaggerAppComponent.create();
-        // Create an InitializerBuilder
         Stetho.InitializerBuilder initializerBuilder =
                 Stetho.newInitializerBuilder(this);
-
-        // Enable Chrome DevTools
         initializerBuilder.enableWebKitInspector(
                 Stetho.defaultInspectorModulesProvider(this)
         );
-
-        // Enable command line interface
         initializerBuilder.enableDumpapp(
                 Stetho.defaultDumperPluginsProvider(this)
         );
-
-        // Use the InitializerBuilder to generate an Initializer
         Stetho.Initializer initializer = initializerBuilder.build();
-
-        // Initialize Stetho with the Initializer
         Stetho.initialize(initializer);
     }
 
